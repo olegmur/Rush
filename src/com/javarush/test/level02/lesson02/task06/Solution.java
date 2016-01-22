fdcba8b97941201d99564c44e4eee600474a972c
olegmur <olegmur@yandex.ru>
2016-01-22 13:53:19 +0300

    123

--- src/com/javarush/test/level02/lesson02/task06/Solution.java
+++ src/com/javarush/test/level02/lesson02/task06/Solution.java
@@ -12,7 +12,7 @@ public class Solution
     public static void main(String[] args) {
         int a=5;
         int b=7;
-     //   //a=5;
+     /////   //a=5;
         //b=7;
         System.out.println(a+b);
         System.out.println(a*b);
